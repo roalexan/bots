ebfe9294565190f8d2fcf8f2cc12dfc08065f12f
Robert Alexander <roalexan@microsoft.com>
2018-09-12 10:33:39 -0400

    move to typescript folder

--- samples/baseecho/javascript/index.js
+++ samples/baseecho/javascript/index.js
@@ -1,17 +1,28 @@
-const { ActivityTypes, BotFrameworkAdapter } = require ('botbuilder');
 const express = require('express');
 const { GREETING, REPLY, USAGE } = require('./text');
+const { CosmosDbTranscriptStore} = require('botbuilder-transcript-cosmosdb');
+const { ActivityTypes, BotFrameworkAdapter, TranscriptLoggerMiddleware } = require('botbuilder');
+//const { DocumentClient } = require('documentdb');
+//import { DocumentClient } from 'documentdb';
+import DocumentClient = require('documentdb');
 
 // Create server
 const server = express();
 const port = process.env.PORT || 3978;
 server.listen(port, () => console.log(`${server.name} listening on ${port}`));
 
+const serviceEndpoint = 'https://rbacosmosdb.documents.azure.com:443/';
+const masterKey = 'lYVOFTeEwSp7CxCjHWc4gywbvx2qebp6N2snZQtbMXSQfbXGeTeDEIS02V74O2olVK6s1QvSeDEJsvqDinWpgg==';
+const client = new DocumentClient(serviceEndpoint, {masterKey});
+const store = new CosmosDbTranscriptStore(client);
+
 // Create adapter
+const logger = new TranscriptLoggerMiddleware(store);
 const adapter = new BotFrameworkAdapter({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD,
-});
+})
+.use(logger);
 
 // Handler for every conversation turn
 const botLogic = async (context) => {
