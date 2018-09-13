38ba735bd438008320b9cbd3d15e0b9de55c51c0
Robert Alexander <roalexan@microsoft.com>
2018-09-13 10:33:21 -0400

    get build to work

--- samples/guessnumberbot/typescript/src/index.ts
+++ samples/guessnumberbot/typescript/src/index.ts
@@ -1,11 +1,11 @@
-import { BotFrameworkAdapter, MemoryStorage, BotContext, ConversationState } from 'botbuilder';
-import * as restify from 'restify';
+//import { BotFrameworkAdapter, MemoryStorage, BotContext, ConversationState } from 'botbuilder';
+import { BotFrameworkAdapter, MemoryStorage, ConversationState, TurnContext, UserState, BotStateSet } from 'botbuilder';
+import * as express from 'express';
 
 // Create HTTP server
-let server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log(`${server.name} listening to ${server.url}`);
-});
+const server = express();
+const port = process.env.PORT || 3978;
+server.listen(port, () => console.log(`${server.name} listening on ${port}`));
 
 // Create adapter
 const adapter = new BotFrameworkAdapter({
@@ -28,7 +28,8 @@ const conversationState = new ConversationState<State>(new MemoryStorage());
 // middleware code that writes back the conversation state at the end of the "turn"
 adapter.use(conversationState);
 
-async function init(context: BotContext) {
+//async function init(context: BotContext) {
+async function init(context: TurnContext) {
   const state = await conversationState.read(context);
   state.low = MIN;
   state.high = MAX;
@@ -37,33 +38,43 @@ async function init(context: BotContext) {
   state.sessionState = 'askReady';
 }
 
-async function showInstructions(context: BotContext) {
+//async function showInstructions(context: BotContext) {
+async function showInstructions(context: TurnContext) {
   const state = await conversationState.read(context);
   await context.sendActivity('Please think of a number between ' + (state.low + 1) + ' and ' + state.high + ' and I will try to guess it.');
 }
 
-async function askReady(context: BotContext) {
+//async function askReady(context: BotContext) {
+async function askReady(context: TurnContext) {
  await context.sendActivity('Type "ready" when you are ready to start.');
 }
 
-async function askPlayAgain(context: BotContext) {
+//async function askPlayAgain(context: BotContext) {
+async function askPlayAgain(context: TurnContext) {
   await context.sendActivity('Would you like to play again?');
 }
 
 // Listen for incoming requests
 server.post('/api/messages', (req, res) => {
   // Route received request to adapter for processing
-  adapter.processRequest(req, res, async (context) => {
-    if (context.request.type === 'conversationUpdate') {
-      for (let member of context.request.membersAdded!) {
-        if (member.id !== context.request.recipient.id) { // check if member id equals the bot's id
+  ////adapter.processRequest(req, res, async (context) => {
+  adapter.processActivity(req, res, async (context) => {
+  ////adapter.processActivity(req, res, botLogic).catch((err) => {
+    //if (context.request.type === 'conversationUpdate') {
+      if (context.activity.type === 'conversationUpdate') {
+      //for (let member of context.request.membersAdded!) {
+      for (let member of context.activity.membersAdded!) {
+        //if (member.id !== context.request.recipient.id) { // check if member id equals the bot's id
+        if (member.id !== context.activity.recipient.id) { // check if member id equals the bot's id
           await context.sendActivity('Welcomeeeee to the guess number bot!');
           await init(context);
         }
       }
     }
-    if (context.request.type === 'message') {
-      let message = (context.request.text || '').trim().toLowerCase();
+   //if (context.request.type === 'message') {
+    if (context.activity.type === 'message') {
+      //let message = (context.request.text || '').trim().toLowerCase();
+      let message = (context.activity.text || '').trim().toLowerCase();
       const state = await conversationState.read(context);
       if (state.sessionState === 'askReady') {
         if (message.startsWith('ready')) {
